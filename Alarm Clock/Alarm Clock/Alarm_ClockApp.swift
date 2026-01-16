0bbd04552a62102712c23170481261e72d8db486
allanilya <allanilyasov@gmail.com>
2026-01-16 12:20:05 -0500

    Integration of apple multiplatform os application. sync with iphonme app works

---
+++ Alarm Clock/Alarm Clock/Alarm_ClockApp.swift
@@ -0,0 +1,17 @@
+//
+//  Alarm_ClockApp.swift
+//  Alarm Clock
+//
+//  Created by Allan on 1/15/26.
+//
+
+import SwiftUI
+
+@main
+struct Alarm_ClockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
